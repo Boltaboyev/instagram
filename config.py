9c3786f3410d55fc51fcc2110e67243048da584a
Jim-Floyd <jamshid.elboev5111995@gmail.com>
2022-05-14 15:02:39 +0500

    sdsd

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, url_for, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -6,7 +7,7 @@ from flask_migrate import *
 import sys
 from flask_script import Manager
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.urandom(24)
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123@localhost/instagram_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
