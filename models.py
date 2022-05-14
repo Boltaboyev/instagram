fccc7a8b3ab6684b9aa2b6218551c15112ba24cc
Bekzod <abduraxmanbekzod679@gmail.com>
2022-05-14 12:56:56 +0300

    first

--- models.py
+++ models.py
@@ -18,33 +18,40 @@ class Users(db.Model):
     name = db.Column(db.String())
     email = db.Column(db.String())
     password = db.Column(db.String())
+    data = db.relationship('User_data', backref="data_owner")
+
+
+class User_data(db.Model):
+    __tablename__ = 'user_data'
+    id = db.Column(db.Integer, primary_key=True)
     img = db.Column(db.String())
     subscribers = db.relationship("Subscribers", backref='subscribers_owner')
     subscriptions = db.relationship('Subscriptions', backref='subscriptions_owner')
     posts = db.relationship("Posts", backref="posts_owner")
+    user = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 
 class Posts(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     post = db.Column(db.String())
-    post_owner = db.Column(db.Integer, db.ForeignKey('users.id'))
-    post_likes = db.relationship("Likes", backref="likes_owner")
+    post_owner = db.Column(db.Integer, db.ForeignKey('user_data.id'))
+    post_like = db.relationship("Likes", backref="likes_owner")
 
 
 class Subscribers(db.Model):
     __tablename__ = 'subscribers'
     id = db.Column(db.Integer, primary_key=True)
-    subscribers_owner = db.Column(db.Integer, db.ForeignKey('users.id'))
+    subscriber_owner = db.Column(db.Integer, db.ForeignKey('user_data.id'))
 
 
 class Likes(db.Model):
     __tablename__ = 'likes'
     id = db.Column(db.Integer, primary_key=True)
-    likes_owner = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    like_owner = db.Column(db.Integer, db.ForeignKey('posts.id'))
 
 
 class Subscriptions(db.Model):
     __tablename__ = 'subscriptions'
     id = db.Column(db.Integer, primary_key=True)
-    subscriptions_owner = db.Column(db.Integer, db.ForeignKey('users.id'))
+    subscription_owner = db.Column(db.Integer, db.ForeignKey('user_data.id'))
